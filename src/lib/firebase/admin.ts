b23776e088579ca90e3e435dfdac4ed27af0ad8b
Gamer <gamertechs.defi@gmail.com>
2026-01-26 23:27:29 +0100

    fix

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -9,7 +9,7 @@ const serviceAccount = JSON.parse(
 
 const newServiceAccount = JSON.parse(
   JSON.stringify({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   })
