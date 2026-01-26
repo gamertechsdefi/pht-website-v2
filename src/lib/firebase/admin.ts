e3dd4fdda80ecd5e647e5846466af47745a22377
Gamer <gamertechs.defi@gmail.com>
2026-01-26 23:05:52 +0100

    fix

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -7,9 +7,17 @@ const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
 );
 
+const newServiceAccount = JSON.parse(
+  JSON.stringify({
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  })
+);
+
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount as any),
+    credential: cert(newServiceAccount),
   });
 }
 
